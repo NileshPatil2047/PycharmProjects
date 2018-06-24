4a5476cc66340faee56ac79eb699455ee098630b
Nilesh Patil <nilesh.job2047@gmail.com>
2018-06-24 23:05:16 +0530

    Added Two Projects

---
+++ Selenium_Parallel_Test/TestCases/test_parallel.py
@@ -0,0 +1,32 @@
+import unittest
+from time import sleep
+from selenium import webdriver
+
+
+class TestParallel(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor= "http://localhost:4444/wd/hub",
+            desired_capabilities={
+                "browserName": "chrome",
+                })
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+
+    def test_four(self):
+        driver = self.driver
+        driver.get("https://www.google.com")
+        sleep(5)
+
+    def test_five(self):
+        driver = self.driver
+        driver.get("https://www.facebook.com")
+        sleep(5)
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
