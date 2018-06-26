48e28649f9fd0c1be748c6fd07ccd2e4770343ad
Nilesh <nilesh.a.patil@tieto.com>
2018-06-26 20:00:01 +0530

    Added Excel Reader

--- FirstSeleniumTest/TestCases/ReadCSV.py
+++ FirstSeleniumTest/TestCases/ReadCSV.py
@@ -1,6 +1,11 @@
 import csv
-
+import win32com.client as win32
 # ----------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+"""
+@author: Nilesh Kumar Patil
+
+"""
 
 
 def csv_dict_reader(file_obj):
@@ -12,6 +17,7 @@ def csv_dict_reader(file_obj):
     for line in reader:
         if line["Task"] == "03.1-On - Development":
             print "INVALID DATA FOUND"
+            send_notification()
         else:
             print "CORRECT DATA FOUND"
 
@@ -19,11 +25,19 @@ def csv_dict_reader(file_obj):
         print(line["Task"])
 
 
+def send_notification():
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = 'Pawan.Kumar@tieto.com'
+    mail.CC = 'nilesh.a.patil@tieto.com'
+    mail.Subject = 'Invalid Data Found <<Sent through Automated email box>>'
+    mail.body = 'This email alert is auto generated. Please do not respond.'
+    mail.send
+
+
 # ----------------------------------------------------------------------
 if __name__ == "__main__":
     with open("C:\\PycharmProjects\\mytime_emp_rh_201806_all_projects.csv") as f_obj:
         csv_dict_reader(f_obj)
-
-
 
 
